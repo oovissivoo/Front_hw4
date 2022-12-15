07f8274f60d81b24eb4c6834a8f76fcb38a96fbf
oovissivoo <nurkanat02@inbox.ru>
2022-12-15 15:26:47 +0500

    1
    first

---
+++ day2_loops.js
@@ -0,0 +1,42 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the vowelsAndConsonants function.
+ * Print your output using 'console.log()'.
+ */
+function vowelsAndConsonants(s) {
+    // vowels
+    for(let ind=0; ind<s. length; ind++){
+        if(s.charAt(ind).match(/[aeiou]/)){
+            console.log(s.charAt(ind));
+        }
+    }
+    // consonants
+    for(let ind=0; ind<s. length; ind++){
+        if(s.charAt(ind).match(/[^aeiou]/)){
+            console.log(s.charAt(ind));
+        }
+    }
+}
